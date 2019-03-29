7d1b84a0b7321dc9e2b97fc5f184a6b3cffab925
MdMahadiaHossain <mahadihossainpvt@gmail.com>
2019-03-29 16:05:00 +0600

    Create code.scala

---
+++ code.scala
@@ -0,0 +1,51 @@
+
+
+object EntryPoint {
+  def main(args: Array[String]): Unit = {
+    implicit def processor[T <: {def execute()}](obj: T): html = {
+      obj.execute()
+      new html {}
+    }
+
+    a {
+      self =>
+        self.href("www.facebook.com")
+        self._class(Seq("container"))
+        new text("")
+    }
+
+  }
+}
+
+class a {
+  def execute(): Unit = {
+    println("processing. . . . scala code to html")
+    println(s"<a href='$url' class='${_class}'>" + "click" + "</a>")
+  }
+
+  private var url = "#"
+  private var _class: Seq[String] = Seq()
+
+
+  def href(urla: String): a = {
+    this.url = urla
+    this
+  }
+
+  def _class(classes: Seq[String]): a = {
+    this._class = classes
+    this
+  }
+}
+
+object a {
+  def apply(f: a => html)(implicit processor: a => html): a = {
+
+    val x = new a()
+    f(x)
+    processor(x)
+    x
+  }
+}
+
+class text(txt: String) extends html
